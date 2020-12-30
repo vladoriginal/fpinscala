17bb94ff30164747308fc1fcf2b57156bfecbb75
MariRiemann <riemann.marina@gmail.com>
2020-12-30 21:15:37 +0300

    Monad 3

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -61,8 +61,6 @@ trait Monad[M[_]] extends Functor[M] {
   def __flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = join(map(ma)(f))
 }
 
-case class Reader[R, A](run: R => A)
-
 //noinspection TypeAnnotation
 object Monad {
   val genMonad = new Monad[Gen] {
@@ -94,20 +92,38 @@ object Monad {
       override def flatMap[A, B](ma: State[S, A])(f: A => State[S, B]): State[S, B] = ma flatMap f
     }
   }
-  val idMonad: Monad[Id] = ???
+  val idMonad: Monad[Id] = new Monad[Id] {
+    override def unit[A](a: => A): Id[A] = Id(a)
+    override def flatMap[A, B](ma: Id[A])(f: A => Id[B]): Id[B] = (f(ma.value))
+  }
 
   def readerMonad[R] = ???
 }
 
-case class Id[A](value: A) {
-  def map[B](f: A => B): Id[B] = ???
-  def flatMap[B](f: A => Id[B]): Id[B] = ???
+case class Id[A](value: A)
+
+case class Reader[R, A](run: R => A) {
+  def flatMap[B](f: A => Reader[R,B]): Reader[R,B] = Reader(r => f(run(r)).run(r))
+//  def mapR[R1](f: R => R1): Reader[R1,A] = Reader(r => run())
+  def getR: Reader[R, R] = Reader(identity)
 }
 
 object Reader {
   def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
-    def unit[A](a: => A): Reader[R,A] = ???
-    override def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] = ???
+    def unit[A](a: => A): Reader[R,A] = Reader(_ => a)
+    def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] = st flatMap f
+  }
+
+  def t(): Unit = {
+//    val f = (r: String) => r.toInt
+//    val ma = Reader(f)
+    val mma = Reader[String, Reader[String, Int]](r => Reader(r2 => (r + r2).toInt))
+    val j = readerMonad.join(mma)
+    println(mma.run("1").run("2"))
+    println(j.run("3"))
   }
 }
 
+object TestMonad extends App {
+  Reader.t()
+}
